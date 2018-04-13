0f1904f6f8b97141ce340341b6470e4a663c5513
Kyle Absten <kyleabsten@Kyles-MacBook.local>
2018-04-13 10:05:05 -0700

    Add .gitignore file

---
+++ hwMos_Project7/Photo.swift
@@ -0,0 +1,18 @@
+//
+//  Photo.swift
+//  hwMos_Project7
+//
+//  Created by Kyle Absten on 4/10/18.
+//  Copyright © 2018 Kyle Absten. All rights reserved.
+//
+
+import Cocoa
+
+class Photo: NSCollectionViewItem {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
